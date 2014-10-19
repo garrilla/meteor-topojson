faa2c2867ef7462951f4f11653e76435a2b5f097
garrilla <garry@garrilla.com>
2014-10-19 11:19:02 +0100

    added version test

--- garrilla:topojson-tests.js
+++ garrilla:topojson-tests.js
@@ -1,5 +1,12 @@
 // Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
-});
+if(Meteor.isClient) {
+  /** calls the d3.version prototype, which returns the version string
+   *  and tests if its the same as the version that is declared here
+   *  TODO change version to string to call from
+   *  this assures that d3 is available, and that it's prototypes
+   *  return values - eveything else we have to assume running based
+   *  d3's own test framework */
+  Tinytest.add('garrilla:topojson version test', function (test) {
+    test.equal(topojson.version, '1.6.18');
+  });
+}
